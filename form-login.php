d9a0816023b6974ee455d56a9c2eb7014ffba35c
nabilajr <46430680+nabilajr@users.noreply.github.com>
2019-03-18 10:24:12 +0700

    Add files via upload

---
+++ form-login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+<h1>Login</h1>
+    <form action="login.php" method="POST">
+    <table>
+    <tr>
+        <td><label>Username</label></td>
+        <td>:</td>
+        <td><input type="text" name="username"></td>
+    </tr>
+    <tr>
+        <td><label>Password</label></td>
+        <td>:</td>
+        <td><input type="password" name="password"></td>    
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td><input type="submit" value="Simpan" name="btnSimpan"></td>    
+    </tr>
+    </table>
+    </form>
+</body>
+</html>
